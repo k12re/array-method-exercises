d904059781a4972710aeafac0b011c97efa57185
Ken Thore <kenthore@live.no>
2023-02-02 13:45:32 +0100

    reduce exercise

--- reduce/exercise-2.js
+++ reduce/exercise-2.js
@@ -20,22 +20,25 @@ the items in stock ('itemsInStock') by the price ('price')
 
 const items = [
   {
-    name: 'Apple',
+    name: "Apple",
     itemsInStock: 10,
     price: 5.99,
   },
   {
-    name: 'Banana',
+    name: "Banana",
     itemsInStock: 5,
     price: 3.99,
   },
   {
-    name: 'Orange',
+    name: "Orange",
     itemsInStock: 2,
     price: 3.99,
   },
 ];
 
-const totalCostOfItems = null; // Replace null and add .reduce code here
+const totalCostOfItems = items.reduce((total, value) => {
+  total += value.itemsInStock * value.price;
+  return total;
+}, 0);
 
 console.log(totalCostOfItems);
