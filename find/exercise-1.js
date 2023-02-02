965fe7c2b4ccf8f2121256069de0dcca16b71b4d
Ken Thore <kenthore@live.no>
2023-02-02 16:06:54 +0100

    find exercise

--- find/exercise-1.js
+++ find/exercise-1.js
@@ -13,6 +13,8 @@ set the result of this to the supplied 'foundIndex' variable.
 const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Find the value of 5:
-const foundIndex = null; // Replace null and add .map code here
+const foundIndex = values.find((value) => {
+  if (value === 5) return true;
+});
 
-console.log(valuesTimesTwo);
+console.log(foundIndex);
